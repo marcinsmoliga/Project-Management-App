a0a70740292dc2a50ed8ed8508db24f49075d97b
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-02-15 19:26:01 +0100

    h2 database properties are set

--- src/main/java/com/example/projectmanagementapp/controller/ProjectController.java
+++ src/main/java/com/example/projectmanagementapp/controller/ProjectController.java
@@ -3,6 +3,7 @@ package com.example.projectmanagementapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.projectmanagementapp.entity.Project;
@@ -17,5 +18,10 @@ public class ProjectController {
 		Project project = new Project();
 		model.addAttribute("project", project);
 		return "new_project";
+	}
+
+	@PostMapping("/save")
+	public String createProject(Project project, Model model) {
+
 	}
 }
