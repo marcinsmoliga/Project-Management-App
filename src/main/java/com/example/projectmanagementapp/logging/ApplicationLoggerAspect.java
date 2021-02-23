3bc6ea4047a717919a6b32c690a85d294ea40ef2
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-02-23 19:43:47 +0100

    ApplicationLoggerAspect -> method logAround(.) implemented

--- src/main/java/com/example/projectmanagementapp/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/example/projectmanagementapp/logging/ApplicationLoggerAspect.java
@@ -1,7 +1,13 @@
 package com.example.projectmanagementapp.logging;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,14 +18,45 @@ import org.springframework.stereotype.Component;
 public class ApplicationLoggerAspect {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Pointcut("within(com.example.projectmanagementapp.controller..*)"
-			   + "|| within(com.example.projectmanagementapp.dao..*)")
+	@Pointcut("within(com.example.projectmanagementapp.controller..*)")
+
 	public void definePackagePointcuts() {
 
 	}
 
 	@After("definePackagePointcuts()")
-	public void log() {
-		log.debug("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+	public void logAfter(JoinPoint joinPoint) {
+		log.debug("\n \n \n");
+		log.debug("***** Before Method Execution ***** \n {}.{} with arguments[s] = {}",
+				joinPoint.getSignature().getDeclaringType(),
+				joinPoint.getSignature().getName(),
+				Arrays.toString(joinPoint.getArgs()));
+		log.debug("________________________________________________________ \n \n \n");
+	}
+
+	@Around("definePackagePointcuts()")
+	public Object logAround(ProceedingJoinPoint joinPoint) {
+		log.debug("\n \n \n");
+		log.debug("***** Before Method Execution ***** \n {}.{} with arguments[s] = {}",
+				joinPoint.getSignature().getDeclaringType(),
+				joinPoint.getSignature().getName(),
+				Arrays.toString(joinPoint.getArgs()));
+		log.debug("________________________________________________________ \n \n \n");
+
+		Object object = null;
+		try {
+			object = joinPoint.proceed();
+		} catch (Throwable throwable) {
+			throwable.printStackTrace();
+		}
+
+		log.debug("\n \n \n");
+		log.debug("***** After Method Execution ***** \n {}.{} with arguments[s] = {}",
+				joinPoint.getSignature().getDeclaringType(),
+				joinPoint.getSignature().getName(),
+				Arrays.toString(joinPoint.getArgs()));
+		log.debug("________________________________________________________ \n \n \n");
+
+		return object;
 	}
 }
