4b2273a34dd2495b1a0d90bb9e20a0f14b8f370c
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-03-12 15:23:04 +0100

    EmployeeAppController -> mehtod findPaginatedEmployees(..) implemented

--- src/main/java/com/example/projectmanagementapp/service/EmployeeService.java
+++ src/main/java/com/example/projectmanagementapp/service/EmployeeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.projectmanagementapp.dao.EmployeeRepository;
@@ -41,5 +42,9 @@ public class EmployeeService {
 
 	public Employee findByEmail(String s) {
 		return  employeeRepository.findByEmail(s);
+	}
+
+	public Iterable<Employee> getPage(Pageable pageAndSize) {
+		return employeeRepository.findAll(pageAndSize);
 	}
 }
