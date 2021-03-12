1ba8c76d2e24ff27885cd741ebd03ad9badbcc2e
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-03-12 15:54:00 +0100

    pom.xml -> Rest Repositories dependency added | EmployeeRepository -> @RepositoryRestResource added

--- src/main/java/com/example/projectmanagementapp/dao/EmployeeRepository.java
+++ src/main/java/com/example/projectmanagementapp/dao/EmployeeRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.example.projectmanagementapp.dto.EmployeeProject;
 import com.example.projectmanagementapp.entity.Employee;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "apiemployees", path = "apiemployees")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 	@Query(nativeQuery = true, value = "SELECT e.first_name AS firstName, e.last_name AS lastName, "
